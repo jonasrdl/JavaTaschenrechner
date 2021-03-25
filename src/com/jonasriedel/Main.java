e02640fb1b7cc01a0918c2f89f4a467871e3ce9e
Jonas Riedel <jonasriedel@gmx.net>
2021-03-25 21:04:45 +0100

    Initial commit

---
+++ src/com/jonasriedel/Main.java
@@ -0,0 +1,12 @@
+package com.jonasriedel;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welche Rechenart möchtest du wählen?");
+    }
+}
