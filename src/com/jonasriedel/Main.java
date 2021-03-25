2abcdcbaa0ba16ac83dbf509022125b528181afd
Jonas Riedel <jonasriedel@gmx.net>
2021-03-25 21:24:54 +0100

    Added addition, subtraction, multiplication and division

--- src/com/jonasriedel/Main.java
+++ src/com/jonasriedel/Main.java
@@ -7,6 +7,51 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welche Rechenart möchtest du wählen?");
+        System.out.println("Welche Rechenart möchtest du wählen? Addieren(+), Subtrahieren(-), Multiplizieren(*) oder Dividieren(/)");
+
+        String input = sc.nextLine();
+
+        if (!input.contains("+") && !input.contains("-") && !input.contains("*") && !input.contains("/") &&
+                !input.toLowerCase().contains("addieren") && !input.toLowerCase().contains("subtrahieren") &&
+                !input.toLowerCase().contains("multiplizieren") && !input.toLowerCase().contains("dividieren")) {
+            System.out.println("`" + input + "´" + " ist keine gültige Rechenart.");
+            return;
+        }
+
+        if (input.equalsIgnoreCase("addieren") || input.equalsIgnoreCase("+")) {
+            System.out.println("Erste Zahl:");
+            int zahl1 = sc.nextInt();
+            System.out.println("Zweite Zahl:");
+            int zahl2 = sc.nextInt();
+            int summe = zahl1 + zahl2;
+            System.out.println("Ergebnis: " + summe);
+        }
+
+        if (input.equalsIgnoreCase("subtrahieren") || input.equalsIgnoreCase("-")) {
+            System.out.println("Erste Zahl:");
+            int zahl1 = sc.nextInt();
+            System.out.println("Zweite Zahl:");
+            int zahl2 = sc.nextInt();
+            int summe = zahl1 - zahl2;
+            System.out.println("Ergebnis: " + summe);
+        }
+
+        if (input.equalsIgnoreCase("multiplizieren") || input.equalsIgnoreCase("*")) {
+            System.out.println("Erste Zahl:");
+            int zahl1 = sc.nextInt();
+            System.out.println("Zweite Zahl:");
+            int zahl2 = sc.nextInt();
+            int summe = zahl1 * zahl2;
+            System.out.println("Ergebnis: " + summe);
+        }
+
+        if (input.equalsIgnoreCase("dividieren") || input.equalsIgnoreCase("/")) {
+            System.out.println("Erste Zahl:");
+            int zahl1 = sc.nextInt();
+            System.out.println("Zweite Zahl:");
+            int zahl2 = sc.nextInt();
+            int summe = zahl1 / zahl2;
+            System.out.println("Ergebnis: " + summe);
+        }
     }
 }
