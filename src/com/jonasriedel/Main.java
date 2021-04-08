c3c51fa3e18034af675dd631af5b05c45d07dc72
Jonas Riedel <jonasriedel@gmx.net>
2021-04-08 14:21:06 +0200

    outsourced both languages in seperate methods, checking if number is too long, added an 'again' option

--- src/com/jonasriedel/Main.java
+++ src/com/jonasriedel/Main.java
@@ -1,5 +1,6 @@
 package com.jonasriedel;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -12,10 +13,21 @@ public class Main {
         String language = sc.nextLine();
 
         if (language.equalsIgnoreCase("deutsch")) {
+            germanCalculator();
+        }
 
-            System.out.println("Welche Rechenart möchtest du wählen? Addieren(+), Subtrahieren(-), Multiplizieren(*) oder Dividieren(/)");
+        if (language.equalsIgnoreCase("english")) {
+            englishCalculator();
+        }
+    }
 
-            String input = sc.nextLine();
+    public static void germanCalculator() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welche Rechenart möchtest du wählen? Addieren(+), Subtrahieren(-), Multiplizieren(*) oder Dividieren(/)");
+
+        String input = sc.nextLine();
+        try {
 
             if (!input.contains("+") && !input.contains("-") && !input.contains("*") && !input.contains("/") &&
                     !input.toLowerCase().contains("addieren") && !input.toLowerCase().contains("subtrahieren") &&
@@ -63,14 +75,34 @@ public class Main {
                 System.out.println(zahl1 + " / " + zahl2 + " = " + summe);
                 System.out.println("Ergebnis: " + summe);
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Zahl zu lang... Nochmal von vorne");
+            germanCalculator();
         }
 
-        if (language.equalsIgnoreCase("english")) {
+        Scanner nochmal = new Scanner(System.in);
 
-            System.out.println("Which type of calculation do you want to choose? Add (+), Subtract (-), Multiply (*) or Divide (/) ");
+        while (true) {
+            System.out.println("Das war toll! Nochmal? J/N.");
 
-            String input = sc.nextLine();
+            String entscheidung = nochmal.nextLine();
 
+            if (entscheidung.equalsIgnoreCase("j")) {
+                germanCalculator();
+            } else if (entscheidung.equalsIgnoreCase("n")) {
+                System.out.println("Wir sehen uns :)");
+                System.exit(0);
+            }
+        }
+    }
+
+    public static void englishCalculator() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Which type of calculation do you want to choose? Add (+), Subtract (-), Multiply (*) or Divide (/) ");
+
+        String input = sc.nextLine();
+        try {
             if (!input.contains("+") && !input.contains("-") && !input.contains("*") && !input.contains("/") &&
                     !input.toLowerCase().contains("add") && !input.toLowerCase().contains("subtract") &&
                     !input.toLowerCase().contains("multiply") && !input.toLowerCase().contains("divide")) {
@@ -116,6 +148,24 @@ public class Main {
                 int summe = zahl1 / zahl2;
                 System.out.println(zahl1 + " / " + zahl2 + " = " + summe);
                 System.out.println("Result: " + summe);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Number too long... Try again!");
+            englishCalculator();
+        }
+
+        Scanner again = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("That was nice! Again? Y/N");
+
+            String choice = again.nextLine();
+
+            if (choice.equalsIgnoreCase("y")) {
+                englishCalculator();
+            } else if (choice.equalsIgnoreCase("n")) {
+                System.out.println("See you :)");
+                System.exit(0);
             }
         }
     }
